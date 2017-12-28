45daf5d22aa2bf42b44fd86113027f279fddd768
Christopher Dorst <chris@dorst.email>
2017-12-28 16:18:48 -0600

    Add GitInitCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitInitCommand/GitInitCommandRunner.cs
@@ -0,0 +1,14 @@
+﻿using static DevOps.VersionControl.Functions.RunGitCommand.GitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitInitCommand
+{
+    public static class GitInitCommandRunner
+    {
+        private const string init = nameof(init);
+
+        public static void Init(string directory, params string[] arguments)
+            => Git(directory,
+                command: init,
+                arguments);
+    }
+}
